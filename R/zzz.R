03c578bca3749112f3af84660447cfb226544c61
Philip Delff <philip@delff.dk>
2020-10-16 15:14:39 -0400

    Welcome message

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,4 @@
-## .onLoad <- function(){
-
-##     message("Welcome to NMdata. Best place to browse documentation is https://philipdelff.github.io/NMdata")
-
-## }
 
 .onAttach <- function(libname,pkgname){
-    packageStartupMessage("Best place to browse NMdata documentation is https://philipdelff.github.io/NMdata")
+    packageStartupMessage("Welcome to NMdata. Best place to browse NMdata documentation is\nhttps://philipdelff.github.io/NMdata")
 }
