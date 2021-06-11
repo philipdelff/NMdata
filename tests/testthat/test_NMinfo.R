448f14a71b41968abc8007126abd78ff579e274b
Philip Delff <philip@delff.dk>
2021-06-11 16:01:46 -0400

    more tests

---
+++ tests/testthat/test_NMinfo.R
@@ -0,0 +1,12 @@
+test_that("basic",{
+
+    fileRef <- "testReference/NMinfo_1.rds"
+
+    file.lst <- system.file("examples/nonmem/xgxr001.lst" ,package="NMdata")
+    res1 <- NMscanData(file=file.lst,quiet=T,order.columns = F,merge.by.row=FALSE)
+
+    info.cols <- NMinfo(res1,info="columns")
+    
+    expect_equal_to_reference(info.cols,fileRef,version=2)
+
+})
--- tests/testthat/test_renameByContents.R
+++ tests/testthat/test_renameByContents.R
@@ -4,3 +4,20 @@
 
 ## pk2 <- renameByContents(pk[,!("trtact")],fun.test=NMisNumeric,fun.rename=tolower,invert.test=T)
 ## pk2
+
+context("renameByContents")
+
+test_that("basic",{
+
+    fileRef <- "testReference/renameByContents_1.rds"
+    
+    pk <- readRDS(file=system.file("examples/data/xgxr2.rds",package="NMdata"))
+    pk[,trtact:=NULL]
+    pk <- renameByContents(data=pk,
+                           fun.test=NMisNumeric,
+                           fun.rename = tolower,
+                           invert.test = TRUE)
+
+    expect_equal_to_reference(pk,fileRef)
+
+})
