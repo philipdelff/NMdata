26cc87f8afb7d8d86c88d61b9e67aed98463b125
Philip Delff <philip@delff.dk>
2021-06-17 20:24:35 -0400

    restructure columns

--- devel/scripts/correct_lsts.R
+++ devel/scripts/correct_lsts.R
@@ -8,24 +8,6 @@ lsts <- list.files(system.file("examples/nonmem",package="NMdata"),pattern="xgxr
 ## }
 
 
-for (lst in lsts){
-
-    lines <- readLines(lst)
-    ## until "$PROBLEM"
-    lines <- lines[-max(1,grep(" *\\$PROBLEM.*",x=lines)[1]-1)]
-
-    ## remove block
-    block.start <- grep("^License Registered to.*",lines)
-    block.end <- grep("^1NONLINEAR MIXED EFFECTS.*",lines)-1
-    if(length(block.start) && length(block.end)){
-        lines <- lines[-(block.start:block.end)]
-
-        ## disregard from "Stop Time:"
-        lines <- lines[1:(min(grep("^ *Stop Time.*",lines))-1)]
-
-        writeLines(lines,con=lst)
-    }
-}
 
 ### complete
 for (lst in lsts){
@@ -43,3 +25,27 @@ for (lst in lsts){
 }
 
 
+
+### only partial stripping
+if(F){
+
+    for (lst in lsts){
+
+        lines <- readLines(lst)
+        ## until "$PROBLEM"
+        lines <- lines[-max(1,grep(" *\\$PROBLEM.*",x=lines)[1]-1)]
+
+        ## remove block
+        block.start <- grep("^License Registered to.*",lines)
+        block.end <- grep("^1NONLINEAR MIXED EFFECTS.*",lines)-1
+        if(length(block.start) && length(block.end)){
+            lines <- lines[-(block.start:block.end)]
+
+            ## disregard from "Stop Time:"
+            lines <- lines[1:(min(grep("^ *Stop Time.*",lines))-1)]
+
+            writeLines(lines,con=lst)
+        }
+    }
+
+}
