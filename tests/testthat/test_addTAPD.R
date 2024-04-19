3fb38cef0413f2cfcb9e50bb50f8835b7538c07b
Philip Delff <philip@delff.dk>
2024-04-19 14:33:20 -0400

    minor fix

--- tests/testthat/test_addTAPD.R
+++ tests/testthat/test_addTAPD.R
@@ -78,13 +78,10 @@ test_that("Repeated doses without samples",{
 if(F){
 ### this example shows most key features. Reduce number of rows and build examples and tests on it.
     
-## library(devtools)
-<<<<<<< HEAD
+    ## library(devtools)
     ## load_all("~/wdirs/NMexec")
-=======
     load_all("~/wdirs/NMsim")
->>>>>>> 4b0847a60b9eead9051e48950e31ee1449a99978
-     NMdataConf(as.fun="data.table")
+    NMdataConf(as.fun="data.table")
     
     doses.1 <- NMcreateDoses(TIME=c(0,3,12,24),CMT=1,AMT=400e3)
     doses.2 <- NMcreateDoses(TIME=0,addl=list(ADDL=4,II=6),CMT=2,AMT=20)
