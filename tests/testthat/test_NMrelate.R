13e939602fe62450a4c00693be14b0cc4c2ebfd5
Philip Delff <philip@delff.dk>
2024-07-06 22:07:52 -0400

    adding test

---
+++ tests/testthat/test_NMrelate.R
@@ -0,0 +1,10 @@
+context("NMrelate")
+source("../../devel/NMrelate.R")
+
+test_that("basic",{
+    file.mod <- "testData/nonmem/xgxr032.mod"
+
+    NMrelate(file.mod,type="eta")
+    NMrelate(file.mod,type="theta")
+    NMrelate(file.mod,type="sigma")
+})
