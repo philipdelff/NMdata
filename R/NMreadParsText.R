8b54dfcb2e8539d8b704575b633acb44ad28254b
Philip Delff <philip@delff.dk>
2024-07-10 11:51:27 -0400

    NMreadParText flexibility

--- R/NMreadParsText.R
+++ R/NMreadParsText.R
@@ -32,9 +32,11 @@
 ##' @export
 
 
-NMreadParsText <- function(file,fields,fields.omega=fields,
+NMreadParsText <- function(file,lines,format,format.omega=format,format.sigma=format.omega,
+                           fields,fields.omega=fields,
                            fields.sigma=fields.omega,
-                           use.theta.nums=FALSE,spaces.split=FALSE,modelname,col.model,as.fun){
+                           use.theta.nums=FALSE,spaces.split=FALSE,
+                           modelname,col.model,as.fun){
 
     idx <- NULL
     par.type <- NULL
@@ -46,19 +48,59 @@ NMreadParsText <- function(file,fields,fields.omega=fields,
     OMEGA <- NULL
     SIGMA <- NULL
 
+    if(missing(file)) file <- NULL
+    if(missing(lines)) lines <- NULL
+    
+    if(!xor(is.null(file),is.null(lines))){
+        stop("Exactly one of file and lines must be provided.")
+    }
+    if(!is.null(file)){
+        if(length(file)>1) {
+            return(rbindlist(lapply(file,NMreadParsText,
+                                    fields=fields,
+                                    fields.omega=fields.omega,
+                                    fields.sigma=fields.sigma,
+                                    use.theta.nums=use.theta.nums,
+                                    spaces.split=spaces.split)))
+        } else {
+            lines <- readLines(file)
+        }
+        
+    }
+    
     if(missing(as.fun)) as.fun <- NULL
     as.fun <- NMdataDecideOption("as.fun",as.fun)
     if(missing(col.model)) col.model <- NULL 
     col.model <- NMdataDecideOption("col.model",col.model)
     if(missing(modelname)) modelname <- NULL
     modelname <- NMdataDecideOption("modelname",modelname)
-    
-    if(missing(fields)){
-        fields <- "%init;%symbol;%num;%label;%unit"
+
+    args <- getArgs()
+
+    ## deprecated since 2024-07-09 - v0.1.7
+    ## format <- deprecatedArg(newarg="format","fields",args=args)
+    ## format.omega <- deprecatedArg(newarg="format.omega","fields.omega",args=args)
+    ## format.sigma <- deprecatedArg(newarg="format.sigma","fields.sigma",args=args)
+    if(!missing(fields)) {
+        .Deprecated(new="format",old="fields")
     }
-    if(is.function(fields)) fields <- fields(file)
-    if(is.function(fields.omega)) fields.omega <- fields.omega(file)
-    if(is.function(fields.sigma)) fields.sigma <- fields.sigma(file)
+    if(!missing(fields.omega)) {
+        .Deprecated(new="format.omega",old="fields.omega")
+    }
+    if(!missing(fields.sigma)) {
+        .Deprecated(new="format.sigma",old="fields.sigma")
+    }
+
+    
+    if(missing(format)){
+        format <- NULL
+    }
+    if(is.null(format)){
+        format <- "%init;%symbol;%num;%label;%unit"
+    }
+    if(is.function(format)) format <- format(lines)
+    if(is.function(format.omega)) format.omega <- format.omega(lines)
+    if(is.function(format.sigma)) format.sigma <- format.sigma(lines)
 
     cleanSpaces <- function(x,double=TRUE,lead=TRUE,trail=TRUE){
         if(double) x <- gsub(paste0(" +")," ",x)
@@ -66,120 +108,92 @@ NMreadParsText <- function(file,fields,fields.omega=fields,
         if(trail) x <- sub(paste0(" +$"),"",x)
         x
     }
-    escape.charclass <- function(x) {
-        ##gsub("([][\\\\^-])", "\\\\\\1", x)
-        gsub("([][\\\\^(\\\\^-])", "\\\\\\1", x)
-    }
 
     
-    ## function to extact however many fields are available in a commented
+    ## function to extact however many format are available in a commented
     ## parameter row
-    fun.get.fields <- function(x,fields){
+    fun.get.fields <- function(x,format){
         
-        ## pat <- gsub("%[a-zA-Z0-9]*","(.*)",fields)
+        ## pat <- gsub("%[a-zA-Z0-9]*","(.*)",format)
 
-        splitters <- fields$splitters
-        splitters.un <- unique(splitters)
-        
-
-
+        splitters <- format$splitters
+        ## splitters.un <- unique(splitters)
         
         nfields.x <- 0
         xleft <- x
-        for(spl in fields$splitters){
-            spl <- escape.charclass(spl)
-            nfields.x <- nfields.x+as.numeric(grepl(spl,x))
-            xleft <- sub(paste0("^[^",spl,"]*",spl),"",xleft,fixed=F)
-        }
-        nfields.x <- nfields.x+1
-
-        
 
         if(F){
-            ## regex <- paste0("[^", paste(escape.charclass(splitters.un),collapse=""), "]")
-            ## gsub(regex, "", x, perl=TRUE)
-            ## gsub("(?m)(^[^(\n]+)[)]|-", "\\1;", x,perl = TRUE)
-            ## gsub("(?m)(^[^(\n]+)[;]|-", "\\1;", x,perl = TRUE)
-### this doesn't work for characters that need escaping like -
-            nfields.x <- nchar(
-                ## splitters.un2 <- gsub("\\-","\\\\\\\\-",splitters.un)
-                ## splitters.un2 <- gsub("\\)","\\\\\\\\)",splitters.un2)
-                gsub(paste0('[^',paste(splitters.un,collapse=""),"]"),"",x)
-                ## gsub(paste0('[^', splitters.un,"]",collapse=""), "", x)
-
-            )+1
+            for(spl in splitters){
+                
+                ## if(!spaces.split) spl <- paste(paste0(strsplit(spl, "")[[1]]," *"),collapse="")
+                ## spl <- escape.charclass(spl)
+                found <- grepl(spl,xleft)
+                ## if(!found) break
+                nfields.x <- nfields.x+as.numeric(found)
+### not working
+                ##xleft <- sub(paste0("^[^",spl,"]*",spl),"",xleft,fixed=F)
+                xleft <- sub(sprintf(".*%s(.*)",spl),"\\1",xleft)
+            }
+            nfields.x <- nfields.x+1
         }
+        ## nsplitters.x <- length(fields$splitters)
+        nsplitters.x <- length(format$splitters)
         
         ## need a regex that matches the number of items found
-        x.left <- x
+        xleft <- x
         items <- c()
-        for(I in 1:nfields.x){
+        
+        for(I in 1:nsplitters.x){
+            spl <- format$splitters[I]
+            spl.raw <- format$splitters.raw[I]
+            ## this.item.all <- sub(sprintf("([^%s]*)%s.*",
+            ##                              splitters[I],
+            ##                              splitters[I]),
+            ##                      "\\1",xleft)
+
+            this.item.all <- sub(sprintf("%s.*",splitters[I]), "", xleft)
             
-            this.item.all <- sub(sprintf("([^%s]*)%s.*",escape.charclass(splitters[I]),
-                                         escape.charclass(splitters[I])),"\\1",x.left)
             ## this.item <- sub("([^ ]*) $","\\1",this.item.all)
             this.item <- cleanSpaces(this.item.all)
+            
+            
+            if(I<nsplitters.x){
+                
+                spl.raw.next <- format$splitters.raw[I]
+                delim.partial <- paste(paste(strsplit(gsub("\\*","",spl.raw.next),"")[[1]],"*",sep=""),collapse="")
+                delim.partial <- escape.charclass(delim.partial)
+                this.item <- sub(sprintf("%s$",delim.partial),"",this.item)
+            }
             items <- c(items,this.item)
-            x.left <- sub(paste0(this.item.all),"",x.left,fixed=TRUE)
-            x.left <- sub(paste0("^",escape.charclass(splitters[I])),"",x.left)
-            if(x.left=="") break
+
+            if(nchar(this.item.all)>0){
+                xleft <- sub(paste0(this.item.all),"",xleft,fixed=TRUE)
+            }
+            xleft <- sub(paste0("^",splitters[I]),"",xleft)
+            ##if(!grepl("[[:alnum:]]",xleft)) xleft <- ""
+            if(xleft=="")  break
         }
         do.call(data.table,as.list(items))
     }
     
     
-    split.fields <- function(fields){
-## split.fields splits the fields format string into the splitters and the variable names 
 
-        
-        ## number of fields defined in fields
-        
-        ## paste0(".*",paste(chars.to.extract,collapse=").*("),".*")
-        ## regex <- paste0(".* [^", escape.charclass(chars.to.extract), "]")
-        ## nfields.string <- nchars( gsub(regex, "", strings.to.search, perl=TRUE))
-        
-        nfields.string <- nchar(gsub("[^%]","",fields))
-        ## number of fields provided in string
-        if(!spaces.split){
-            fields <- gsub(" +","",fields)
-        }
-        splitters <- gsub("%[[:alnum:]]+","",fields)
-        splitters <- strsplit(splitters, "")[[1]]
-        ## splitters.un <- unique(splitters)
-        
-        string.left <- fields
-        fields.res <- c()
-        ## adding an extra splitter for the last regex to find match
-        splitters.tmp <- c(splitters,";")
-        splitters.tmp <- escape.charclass(splitters.tmp)
-        
 
-        for(I in 1:nfields.string){
-            ## cat(I,"\n")
-            fields.res <- c(fields.res,
-                            sub(
-                                paste0(" *%*([^",splitters.tmp[I],"]*)",splitters.tmp[I],"*.*")
-                               ,"\\1",string.left)
-                            )
-            string.left <- sub(sprintf("[^%s]*%s",splitters.tmp[I],splitters.tmp[I]),"",string.left)
-        }
-        
-        list(fields=fields.res,splitters=splitters)
+
+    get.theta.comments <- function(lines,section,format,use.theta.nums=FALSE){
+
+        res.fields.theta <- splitFields(format,spaces.split=spaces.split)
+        get.comments(lines,section,res.fields=res.fields.theta,use.theta.nums=FALSE)
     }
 
-
-    get.theta.comments <- function(file,section,fields,use.theta.nums=FALSE){
-
-        res.fields.theta <- split.fields(fields)        
-        get.comments(file,section,res.fields=res.fields.theta,use.theta.nums=FALSE)
-    }
-    
-    get.comments <- function(file,section,res.fields,use.theta.nums=FALSE){
+### applies processed fields on control stream sections. It does so by calling fun.get.fields
+    get.comments <- function(lines,section,res.fields,use.theta.nums=FALSE){
         
         ## get theta comments
 ### due to a bug in NMreadSection in NMdata 0.1.3 we need to run this in two steps with keep.comments=TRUE and then remove comments lines
-        lines.thetas <- NMreadSection(file=file,section=section,keep.name=FALSE,keep.empty=FALSE,keep.comments=TRUE)
-        
+        lines.thetas <- NMreadSection(lines=lines,section=section,keep.name=FALSE,keep.empty=FALSE,keep.comments=TRUE)
+        if(length(lines.thetas)==0) return(NULL)
+        ## this should be the same as switching keep.comments to FALSE in NMreadSection()
         lines.thetas <- sub(pattern="^ *;.*$",replacement="",x=lines.thetas)
         ## these will confuse in omega/sigma sections with the current method. For those, numbering has to be done if off-diag elements are defined.
         lines.thetas <- gsub("BLOCK(.+)","",lines.thetas)
@@ -197,11 +211,11 @@ NMreadParsText <- function(file,fields,fields.omega=fields,
         }
     }
 
-    get.omega.comments <- function(file,section,fields){
+    get.omega.comments <- function(lines,section,format){
         
-        res.fields <- split.fields(fields)
-        omegas <- get.comments(file=file,section=section,res.fields=res.fields)
-
+        res.fields <- splitFields(format,spaces.split=spaces.split)
+        omegas <- get.comments(lines=lines,section=section,res.fields=res.fields)
+        if(is.null(omegas)) return(NULL)
         if(!"num"%in%colnames(omegas)) omegas[,num:=.I]
         omegas[,i:=NA_integer_]
         omegas[,j:=NA_integer_]
@@ -214,36 +228,91 @@ NMreadParsText <- function(file,fields,fields.omega=fields,
         omegas
     }
 
-
-
     
-    if(length(file)>1) return(rbindlist(lapply(file,NMreadParsText,
-                                               fields=fields,
-                                               fields.omega=fields.omega,
-                                               fields.sigma=fields.sigma,
-                                               use.theta.nums=use.theta.nums,
-                                               spaces.split=spaces.split)))
-    
-    
-    thetas <- get.theta.comments(file=file,section="THETA",fields=fields,use.theta.nums=use.theta.nums)
-    omegas <- get.omega.comments(file=file,section="omega",fields=fields.omega)
-    sigmas <- get.omega.comments(file=file,section="sigma",fields=fields.sigma)
+    thetas <- get.theta.comments(lines=lines,section="THETA",format=format,
+                                 use.theta.nums=use.theta.nums)
+    omegas <- get.omega.comments(lines=lines,section="omega",format=format.omega)
+    sigmas <- get.omega.comments(lines=lines,section="sigma",format=format.sigma)
 
     ## collect thetas and omegas
     pt1 <- rbind(thetas,omegas,sigmas,fill=T)
-    ## we are aligning with Nonmem's behavior in ext files. The
+    ## we are aligning with Nonmem's behavior in ext liness. The
     ## off-diags are lower triangle. In other words, OMEGA and SIGMA
     ## are specified by column, not by row.
     pt1[par.type%in%cc(OMEGA,SIGMA),`:=`(i=j,j=i)]
 
-    ### it is inconsistent, but this is how it is reported in NMreadExt
+### it is inconsistent, but this is how it is reported in NMreadExt
     pt1[par.type%in%cc(THETA),parameter:=sprintf("THETA%d",i)]
     pt1[par.type%in%cc(OMEGA,SIGMA),parameter:=sprintf("%s(%d,%d)",par.type,i,j)]
-   
     
-    this.model <- modelname(file)
-    pt1[,(col.model):=this.model]
+    
+    if(!is.null(file)){
+        this.model <- modelname(file)
+        pt1[,(col.model):=this.model]
+    }
     
     as.fun(pt1)
 }
 
+
+##' @keywords internal
+escape.charclass <- function(x) {
+    ##gsub("([][\\\\^-])", "\\\\\\1", x)
+    gsub("([][\\\\^(\\\\^-])", "\\\\\\1", x)
+}
+
+
+##' splitFields splits the fields format string into the splitters
+##' and the variable names. NB, this is interpreting the user-provided
+##' fields, it is not looking at control stream text,
+##' @keywords internal
+splitFields <- function(format,spaces.split=FALSE){
+
+
+    
+    ## number of fields defined in format
+    
+    ## paste0(".*",paste(chars.to.extract,collapse=").*("),".*")
+    ## regex <- paste0(".* [^", escape.charclass(chars.to.extract), "]")
+    ## nfields.string <- nchars( gsub(regex, "", strings.to.search, perl=TRUE))
+    
+    nfields.string <- nchar(gsub("[^%]","",format))
+    ## number of fields provided in string
+    if(!spaces.split){
+        format <- gsub(" +","",format)
+    }
+    
+    ## a string where variables are not %var but just % so we can find splitters.
+    ## splitters <- gsub("%[[:alnum:]]+","",fields)
+    string.splitters <- gsub("%[[:alnum:]]+","%",format)
+    splitters <- strsplit(string.splitters,"%")[[1]][-1]
+    
+    if(!spaces.split) splitters <- unlist(lapply(strsplit(splitters, ""),function(x)paste(paste0(x," *"),collapse="")))
+    ## adding an extra splitter for the last regex to find match
+    splitters <- c(splitters,";")
+    splitters.raw <- splitters
+    splitters <- escape.charclass(splitters)
+
+    
+    string.vars <- gsub("[^[:alnum:]]+",";",format)
+    vars <- strsplit(string.vars,";")[[1]][-1]
+
+    ## string.left <- fields
+    ## fields.res <- c()
+    ## ## adding an extra splitter for the last regex to find match
+    ##
+    ## splitters.tmp <- escape.charclass(splitters.tmp)
+    
+
+    ## for(I in 1:nfields.string){
+    ##     ## cat(I,"\n")
+    ##     fields.res <- c(fields.res,
+    ##                     sub(
+    ##                         paste0(" *%*([^",splitters.tmp[I],"]*)",splitters.tmp[I],"*.*")
+    ##                        ,"\\1",string.left)
+    ##                     )
+    ##     string.left <- sub(sprintf("[^%s]*%s",splitters.tmp[I],splitters.tmp[I]),"",string.left)
+    ## }
+    
+    list(fields=vars,splitters=splitters,splitters.raw=splitters.raw)
+}
--- tests/testthat/test_NMreadParText.R
+++ tests/testthat/test_NMreadParText.R
@@ -1,4 +1,17 @@
-context("NMreadExt")
+
+context("splitFields")
+test_that("basic",{
+
+    fileRef <- "testReference/splitFields_01.rds"
+
+    res <- split.fields("%init;[%num2];%symbol")
+    expect_equal_to_reference(res,fileRef)
+
+})
+
+
+
+context("NMreadParText")
 
 readRef <- FALSE
 
@@ -35,4 +48,102 @@ test_that("merge with NMreadExt output",{
 
     expect_equal_to_reference(res,fileRef)
     
+})
+
+
+test_that("complex delimiters",{
+
+    fileRef <- "testReference/NMreadParText_04.rds"
+
+    NMdataConf(reset=T)
+    NMdataConf(as.fun="data.table")
+
+    text <- c("
+; matches format
+$THETA  (.1)             ;[1]; LTVKA
+; missing field
+$THETA  (3)             ;  [] ;LTVV2
+; missing end field
+$THETA  (1)             ;[3]
+; extra delim
+$THETA  (4)             ;[4] ;LTVV3 ;
+; missing end field but has delim
+$THETA  (-1)             ; [5] ;
+
+$OMEGA 0 FIX 
+
+$SIGMA 0 FIX
+
+")
+    lines <- strsplit(text,split="\n")[[1]]
+
+    res <- NMreadParsText(lines=lines,format="%init;[%num2];%symbol")
+res
+
+    expect_equal_to_reference(res,fileRef)
+
+
+})
+
+
+
+test_that("No SIGMA",{
+
+    fileRef <- "testReference/NMreadParText_05.rds"
+
+    NMdataConf(reset=T)
+    NMdataConf(as.fun="data.table")
+
+    text <- c("
+; matches format
+$THETA  (.1)             ;[1]; LTVKA
+
+$OMEGA 0 FIX 
+
+")
+    lines <- strsplit(text,split="\n")[[1]]
+
+    res <- NMreadParsText(lines=lines,format="%init;[%num2];%symbol")
+
+
+    expect_equal_to_reference(res,fileRef)
+
+
+})
+
+
+test_that("Complex OMEGA",{
+
+    fileRef <- "testReference/NMreadParText_06.rds"
+
+    NMdataConf(reset=T)
+    NMdataConf(as.fun="data.table")
+
+    text <- c("
+; matches format
+$THETA  (.1)             ;[1]; LTVKA
+
+$OMEGA 0 FIX ; fake
+$OMEGA  BLOCK(4)
+0.126303  ;    IIV.CLP  ; lognormalOm ;1   ;IIV     ;Between-subject variability on CLP;-
+  0.024  ; IIV.CLP.V2P.cov  ; lognormalOm ;1-2 ;IIV     ;Covariance of BSV on CLP and V2P;-
+  0.127  ;    IIV.V2P  ; lognormalOm ;2   ;IIV     ;Between-subject variability on V2P;-
+  0.2  ; IIV.CLP.V3P.cov  ; lognormalOm ;1-3 ;IIV     ;Covariance of BSV on CLP and V3P;-
+  0.2  ; IIV.V2P.V3P.cov  ; lognormalOm ;2-3 ;IIV     ;Covariance of BSV on V2P and V3P;-
+  0.38  ;    IIV.V3P  ; lognormalOm ;3   ;IIV     ;Between-subject variability on V3P;-
+  0.3  ; IIV.CLP.V3M.cov  ; lognormalOm ;1-4 ;IIV     ;Covariance of BSV on CLP and V3M;-
+  0.2  ; IIV.V2P.V3M.cov  ; lognormalOm ;2-4 ;IIV     ;Covariance of BSV on V2P and V3M;-
+  0.59  ; IIV.V3P.V3M.cov  ; lognormalOm ;3-4 ;IIV     ;Covariance of BSV on V3P and V3M;-
+  0.4  ;    IIV.V3M  ; lognormalOm ;4   ;IIV     ;Between-subject variability on V3M;-
+
+$SIGMA 1
+")
+    lines <- strsplit(text,split="\n")[[1]]
+
+    res <- NMreadParsText(lines=lines,format="%init;[%num2];%symbol",format.omega="%init            ; %symbol            ; %trans       ; %num ; %panel   ; %label ; %unit")
+
+res
+    expect_equal_to_reference(res,fileRef)
+
+
 })
