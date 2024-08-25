432feb2058ca7f075b1da78394afc4ef10af663d
Philip Delff <philip@delff.dk>
2024-08-25 15:07:00 -0400

    only fail on comma in data values if writing csv

--- R/NMwriteData.R
+++ R/NMwriteData.R
@@ -301,13 +301,16 @@ NMwriteData <- function(data,file,formats.write=c("csv","rds"),
 
     ## Check if character variables contain commas
     ## This would cause trouble when writing csv
-    
-    has.no.comma <- data.dt[,lapply(.SD,function(x){is.numeric(x)||!any(grepl(",",as.character(x)))})]
-    comma.ok <- as.logical(has.no.comma[1])
+    if(write.csv){    
+        has.no.comma <- data.dt[,lapply(.SD,function(x){is.numeric(x)||!any(grepl(",",as.character(x)))})]
+        comma.ok <- as.logical(has.no.comma[1])
 
-    if(any(!comma.ok)){
-        messageWrap(paste("You must avoid commas in data values. They will corrupt the csv file, so get rid of them before saving data. Comma found in column(s):",paste(colnames(data.dt)[comma.ok==FALSE],sep=", ")),
-                    fun.msg=stop)
+        if(any(!comma.ok)){
+
+            messageWrap(paste("When writing csv, You must avoid commas in data values. They will corrupt the csv file. Either 1) avoid csv using `formats.write="rds"` or 2) drop columns containing commas or 3) remove or replace commas in data values before saving data. For 3) see `?editCharCols`.\nComma found in column(s):",paste(colnames(data.dt)[comma.ok==FALSE],sep=", ")),
+                        fun.msg=stop)
+        }
+        
     }
 
     
